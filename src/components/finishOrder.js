804fbf4f8d0cd58b6681430b0f95251546caabd4
herick <herickrossato@gmail.com>
2020-10-09 17:13:08 -0300

    remove debug

--- src/components/finishOrder.js
+++ src/components/finishOrder.js
@@ -3,7 +3,6 @@ import TopBar from './topBar';
 import ReviseOrder from './reviseOrder';
 import { Link } from 'react-router-dom';
 export default function FinishOrder(props){
-    console.log(props.selectedItems);
     var url = encodeURIComponent(`Olá, gostaria de fazer o pedido:`);
     var totalPrice = 0.0;
     props.selectedItems.forEach(item =>{
